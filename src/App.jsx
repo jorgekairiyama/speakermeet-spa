27966859678c87fbde3d00e65ae24e0a16ddbfbb
jkairiyama <sochorii@gmail.com>
2025-04-21 19:45:19 -0300

    callbacks

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,16 @@ const App = () =>
     },
   ];
 
+  const handleSearch = (event) =>
+  {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
       <List list={stories} />
@@ -61,13 +66,15 @@ const Item = (props) => (
   </li>
 )
 
-const Search = () =>
+const Search = (props) =>
 {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) =>
   {
     setSearchTerm(event.target.value);
+
+    props.onSearch(event);
   };
 
   return (
