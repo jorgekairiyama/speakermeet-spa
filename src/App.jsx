4e266278c8122aa01cea41be507fd18a5c7f65f0
jkairiyama <sochorii@gmail.com>
2025-04-21 21:13:11 -0300

    v0.0.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,35 @@ const App = () =>
       points: 5,
       objectID: 1,
     },
+    {
+      title: 'React Native',
+      url: 'https://reactnativejs.js.org/',
+      author: 'Dan Clark, Andrew Abramov',
+      num_comments: 2,
+      points: 5,
+      objectID: 2,
+    },
+    {
+      title: 'Nockoutjs',
+      url: 'https://nockoutjs.js.org/',
+      author: 'Isaac Abramov',
+      num_comments: 1,
+      points: 7,
+      objectID: 3,
+    },
   ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleSearch = (event) =>
   {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter(function (story)
+  {
+    return story.title.includes(searchTerm);
+  });
 
   return (
     <div>
@@ -37,7 +60,7 @@ const App = () =>
       <Search onSearch={handleSearch} />
 
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
 
     </div>
   );
@@ -66,27 +89,12 @@ const Item = (props) => (
   </li>
 )
 
-const Search = (props) =>
-{
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = (event) =>
-  {
-    setSearchTerm(event.target.value);
-
-    props.onSearch(event);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
-    </div>
-  );
-}
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+)
 
 
 
